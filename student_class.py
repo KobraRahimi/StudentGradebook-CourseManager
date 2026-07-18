d04a6f90445f1286f9f2a028525da27b33536969
KobraRahimi <kobra010rahimi@gmail.com>
2026-07-18 17:47:27 +0330

    Update student_class.py
    Edit the courses attribute and add docstrings

--- student_class.py
+++ student_class.py
@@ -1,29 +1,32 @@
 class Student:
-    def __init__(self, student_id, name, email, courses):
+    def __init__(self, student_id, name, email):
         self.student_id = student_id
         self.name = name
         self.email = email
-        self.courses = courses
+        self.courses = []
 
     def get_id(self):
+        """Returns the student's ID"""
         return self.student_id
 
     def get_name(self):
+        """Returns the student's name"""
         return self.name
 
     def set_email(self, email):
+        """Updates the student’s email address. It can check that the email is valid before changing it"""
         if email.endswith("@gmail.com"):
             self.email = email
         else:
             print("Invalid email")
 
     def enroll_course(self, course_code):
-        list_courses = [self.courses, course_code]
-        self.courses = list_courses
+        """Adds a course code to the student’s courses list."""
+        self.courses.append(course_code)
 
     def display_info(self):
+        """Shows the student’s basic information, such as ID, name, email, and enrolled courses"""
         print(f"Student ID: {self.student_id}")
         print(f"Name: {self.name}")
         print(f"Email: {self.email}")
         print(f"Courses: {self.courses}")
-
