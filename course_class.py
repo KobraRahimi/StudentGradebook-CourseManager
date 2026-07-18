866b27b7bd4584240ba53e4421b0db9863a8f41b
KobraRahimi <kobra010rahimi@gmail.com>
2026-07-18 18:58:42 +0330

    Update course_class.py
    Edit students and assessments , add docstrings

--- course_class.py
+++ course_class.py
@@ -1,28 +1,30 @@
 class Course:
-    def __init__(self, course_code, course_name, students, assessments):
+    def __init__(self, course_code, course_name):
         self.course_code =course_code
         self.course_name = course_name
-        self.students = students
-        self.assessments = assessments
+        self.students = []
+        self.assessments = []
 
     def add_student(self, student_id):
-        list_students = [self.students, student_id]
-        self.students = list_students
+        """Enrolls a student in this course by adding the student’s ID to the course’s students list"""
+        self.students.append(student_id)
 
     def add_assessment(self, assessment):
-        self.assessments = [self.assessments, assessment]
+        """Adds a quiz, exam, or project to the course"""
+        self.assessments.append(assessment)
 
     def find_assessment(self, title):
-        if title in self.assessments:
+        """Searches for an assessment by title"""
+        if title.lower() or title.title() in self.assessments:
             print(f"The {title} was found.")
         else:
             print(f"{title} not found.")
 
     def display_info(self):
+        """Shows course details, such as course code, course name, number of students, and assessments"""
         print(f"Course Code: {self.course_code}")
         print(f"Course Name: {self.course_name}")
         print(f"Enrolled Students: {len(self.students)}")
         print("Assessments: ")
         # print(f"- Quiz 1 / Max Score: 10 \n- Midterm Exam / Max Score: 100 \n- Final Project / Max Score: 100")
-
 
